5417d0c463b227a56229023cf6070895ad489220
Hafsawane <aludra000@gmail.com>
2023-07-21 18:11:32 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,15 @@
 void print_number(int n)
 {
 unsigned int num = n;
+/* first check if it is negative*/
 if (n < 0)
 {
 putchar('-');
 num = -num;
 }
+/*print the first digits*/
 if ((num / 10) > 0)
 print_number(num / 10);
+/*print the last digit*/
 putchar((num % 10) + 48);
 }
