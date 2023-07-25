d8afe3744f981d698b686d530cd8eb222daf8dc4
Hafsawane <aludra000@gmail.com>
2023-07-24 17:38:01 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen -returns the length of a string
+* @s: string parameter
+* Return: string Length
+*/
+int _strlen(char *s)
+{
+int j;
+for (j = 0; s[j] != '\0'; j++)
+{
+continue;
+}
+return (j);
+}
