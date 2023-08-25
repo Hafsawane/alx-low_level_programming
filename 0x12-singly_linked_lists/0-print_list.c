1cf9665eba6fbc79c4dbdb376b4a43ce9532b08e
Hafsawane <aludra000@gmail.com>
2023-08-25 16:32:05 -0700

    omg

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t s = 0;
-if (h == NULL)
-	return (0);
+size_t i = 0;
 	while (h)
 	{
 		if (!h->str)
@@ -19,8 +17,8 @@ if (h == NULL)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		i++;
 	}
 
-	return (s);
+	return (i);
 }
