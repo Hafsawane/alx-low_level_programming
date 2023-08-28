82d63795606fe14dd403686bd86cde1a5bb924f4
Hafsawane <aludra000@gmail.com>
2023-08-27 17:56:00 -0700

    it is what it is

---
+++ 0x09-static_libraries/100-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  print_times_table - hieheieei
+ *  @n: is amonster
+ */
+void print_times_table(int n)
+{
+int i, j;
+if (n <= 15 && n >= 0)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+if (j != 0)
+printf("%3d", i * j);
+else
+printf("0");
+if (j != n)
+printf(", ");
+}
+printf("\n");
+}
+}
+}
