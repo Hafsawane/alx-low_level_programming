8c23f84b7e485a3d7f34be11b3c470c3fe5702c5
Hafsawane <hafsahafousa1969@gmail.com>
2023-10-04 18:10:21 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	unsigned int i = 0, len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
@@ -23,6 +23,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	i = 0;
+	j = 0;
 
 	if (s1)
 	{
@@ -32,6 +33,17 @@ char *str_concat(char *s1, char *s2)
 			i++;
 		}
 	}
-	return (0);
-}
 
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
+
+	return (s3);
+}
