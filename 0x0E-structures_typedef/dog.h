cb9a68852c5c9d91a307a416cbefc4d19d9dc16a
Hafsawane <hafsahafousa1969@gmail.com>
2023-10-11 12:33:24 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
-
 /**
  * struct dog - a dog's basic info
  * @name: dog s name
@@ -17,13 +15,9 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - typedef for struct dog
- *
- *
- */
 
 typedef struct dog dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
