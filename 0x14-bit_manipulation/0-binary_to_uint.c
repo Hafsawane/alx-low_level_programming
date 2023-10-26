60f3b3234d2ac34fb27ec066c3637af1268a4c4d
Hafsawane <hafsahafousa1969@gmail.com>
2023-10-26 15:05:29 -0700

    ****

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_to_uint - convert a binary number to an unsigned int
- * @b: string of type char
+ * @b: string of type chr
  * Return: converted decimal number or 0 if there is an unconvertable char
  */
 
@@ -11,14 +11,17 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int number;
 
-	if (b == NULL)
-	return (0);
-	while (*b)
+for (int i = 0; b[i] != '\0'; i++)
 {
-	if (*b != '0' && *b != '1')
-
-		return (0);
-		number = number * 2 + (*b++ - '0');
+number = number << 1;
+if (b[i] == '1')
+{
+number = number | 1;
 }
-			return (number);
+else if (b[i] != '0')
+{
+return (0);
+}
+}
+return (number);
 }
