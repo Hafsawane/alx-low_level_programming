67fa62c170cb53060e8616046bc1fb0039130240
Hafsawane <hafsahafousa1969@gmail.com>
2023-10-26 15:08:18 -0700

    ****

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,17 +11,13 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int number;
 
-for (int i = 0; b[i] != '\0'; i++)
-{
-number = number << 1;
-if (b[i] == '1')
-{
-number = number | 1;
-}
-else if (b[i] != '0')
-{
+if (!b)
 return (0);
-}
+while (*b)
+{
+if (*b != '0' && *b != '1')
+return (0);
+number = number * 2 + (*b++ - '0');
 }
 return (number);
 }
